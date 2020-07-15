7e9e5ddfe41313d73fcc4b326ba00c21d1e5c4c8
Jesus Guerrero <jesseguerrero1991@gmail.com>
2020-07-15 14:03:25 -0500

    clearer logging

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
     #Threaded option to enable multiple instances for multiple user access support
     #If its windows, we are probably debugging at home.
     if "win" in os.sys.platform:
-        app.run(threaded=True, port=5000, debug = True)
+        app.run(threaded=True, port=5000, debug = False)
     else:
         app.run(host = '0.0.0.0', port=80, threaded=True, debug=False)
 
