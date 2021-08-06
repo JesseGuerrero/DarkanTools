8970b0950fa188a48fa19fe0466c4742a46e1297
jesse <jesseguerrero1991@gmail.com>
2021-08-05 20:11:38 -0500

    Amplified sound volume

--- static/js/misc.js
+++ static/js/misc.js
@@ -1,3 +1,21 @@
 function playAudio(url) {
-    new Audio(url).play();
+    var audio = new Audio(url);
+    audio = amplifyMedia(audio, 20);
+    audio.media.play();
+}
+
+function amplifyMedia(mediaElem, multiplier) {
+  var context = new (window.AudioContext || window.webkitAudioContext),
+      result = {
+        context: context,
+        source: context.createMediaElementSource(mediaElem),
+        gain: context.createGain(),
+        media: mediaElem,
+        amplify: function(multiplier) { result.gain.gain.value = multiplier; },
+        getAmpLevel: function() { return result.gain.gain.value; }
+      };
+  result.source.connect(result.gain);
+  result.gain.connect(context.destination);
+  result.amplify(multiplier);
+  return result;
 }
